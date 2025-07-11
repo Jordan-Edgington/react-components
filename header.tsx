cd7179400b25405ab17a0112483e00688e8927cd
Jordan Edgington <jordan.edgington.dev@gmail.com>
2025-07-11 08:46:10 -0400

    update header to use spaces not tabs

--- header.tsx
+++ header.tsx
@@ -5,9 +5,9 @@ import { useState } from "react";
 import { Menu } from "lucide-react";
 
 type HeaderProps = {
-	title: string;
-	navigationButtons: JSX.Element[];
-	className?: string;
+  title: string;
+  navigationButtons: JSX.Element[];
+  className?: string;
 };
 
 /*
@@ -27,47 +27,47 @@ const navigationButtons = [
 */
 
 export function Header({ title, navigationButtons, className }: HeaderProps) {
-	return (
-		<header
-			className={`flex justify-between items-center sticky top-0 bg-background shadow-md z-50 p-4 gap-2 ${className}`}
-		>
-			<p className="text-2xl font-bold">{title}</p>
-			<div className="flex gap-4">
-				<Navigation buttons={navigationButtons} />
-				<CondensedNavigation buttons={navigationButtons} />
-			</div>
-		</header>
-	);
+  return (
+    <header
+      className={`flex justify-between items-center sticky top-0 bg-background shadow-md z-50 p-4 gap-2 ${className}`}
+    >
+      <p className="text-2xl font-bold">{title}</p>
+      <div className="flex gap-4">
+        <Navigation buttons={navigationButtons} />
+        <CondensedNavigation buttons={navigationButtons} />
+      </div>
+    </header>
+  );
 }
 
 function Navigation({ buttons }: { buttons: JSX.Element[] }) {
-	return (
-		<span className="hidden sm:flex xs:flex-row xs:items-center xs:gap-4">
-			{buttons}
-		</span>
-	);
+  return (
+    <span className="hidden sm:flex xs:flex-row xs:items-center xs:gap-4">
+      {buttons}
+    </span>
+  );
 }
 
 function CondensedNavigation({ buttons }: { buttons: JSX.Element[] }) {
-	const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-	return (
-		<span className="relative sm:hidden">
-			<Button
-				variant="ghost"
-				onClick={() => {
-					console.log(isOpen);
-					setIsOpen(!isOpen);
-				}}
-				className="p-2"
-			>
-				<Menu className="h-5 w-5" />
-			</Button>
-			{isOpen && (
-				<Card className="absolute top-full right-0 z-50 gap-1 p-1">
-					{buttons}
-				</Card>
-			)}
-		</span>
-	);
+  return (
+    <span className="relative sm:hidden">
+      <Button
+        variant="ghost"
+        onClick={() => {
+          console.log(isOpen);
+          setIsOpen(!isOpen);
+        }}
+        className="p-2"
+      >
+        <Menu className="h-5 w-5" />
+      </Button>
+      {isOpen && (
+        <Card className="absolute top-full right-0 z-50 gap-1 p-1">
+          {buttons}
+        </Card>
+      )}
+    </span>
+  );
 }
